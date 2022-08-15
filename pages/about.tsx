1983188ee13514fccb77b63cbe8806021b6e87bf
Raymond <raynoel@gmail.com>
2022-08-14 22:48:20 -0400

    premier commit

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import Layout from '../components/Layout';
+import Head from 'next/head';
+
+const About = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h1>About Page</h1>
+    </Layout>
+  );
+};
+
+export default About;
